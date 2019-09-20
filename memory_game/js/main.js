2087e1364bc97f834fcb2ec09368f5f3d1e1f3bd
Israa Shuri <asoo@Israas-MacBook-Pro.local>
2019-09-20 15:54:29 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,34 @@
-console.log("Up and running!");
+//console.log("Up and running!");
 
-var cardOne = "queen";
+//alert('Hello, friends.');
+
+const cards = ["queen", "queen", "king", "king"];
+
+//deleted
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardThree);
+console.log("User Flipped " + cardThree);*/
+
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardTwo);
+
+if(cardsInPlay === 2){
+
+}
+
+if(cardsInPlay[0] === cardsInPlay[1]){
+	alert("You found a match!");
+}else{
+	alert("Sorry, try again.");
+}
