c319fa2ba171e35385d79f1ae45e8184b51d7f67
Israa Shuri <asoo@Israas-MacBook-Pro.local>
2019-09-22 12:01:25 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,5 @@
-//Writing code for memory card game
 
-//console.log("Up and running!");
-
-//alert('Hello, friends.');
-
-//Array that contain cards
-//const cards = ["queen", "queen", "king", "king"];
+//array of objects
 const cards = [
 {
 	rank: "queen",
@@ -29,18 +23,51 @@ const cards = [
 }
 ];
 
+//empty array 
+const randomCards = [] ;
 
-//Array to store the cards user filps
-const cardsInPlay = [];
 
-function checkForMatch(){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}else{
-		alert("Sorry, try again.");}
+// this function create random numbers from 1 to 4 to load random cards
+// assign the new elements to randomCards array end empty cards array
+// copy randomCards array elements to cards arrsy by pushing its elements to cards
+function createRandom(){
+
+	while(cards.length !== 0){
+		var randomIndex = Math.floor(Math.random() * cards.length);
+		randomCards.push(cards[randomIndex]);
+		cards.splice(randomIndex , 1);
+	};
+	console.log(cards.length);
+	console.log(randomCards.length);
+
+	var i= randomCards.length;
+	while(randomCards.length !== 0){
+
+		i= randomCards.length -1;
+		cards.push(randomCards[i]);
+		randomCards.splice(i , 1);
+		
+	};
+	console.log(cards.length);
+	console.log(randomCards.length);
 }
 
-//function tell wtich card user flip
+//empty array will contain the two flipped cards
+const cardsInPlay = [];
+
+//function display message to user and sum score 
+var score = 0;
+function checkForMatch(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		score += 10;
+		alert("You found a match!" + "\nYour Score: " + score);
+	}else{
+		score -= 5;
+		alert("Sorry, try again." + "\nYour Score: " + score);
+	}
+}
+
+//function tell wtich card user flipped
 function flipCard (){
 
 	//this = cardElement = element the user clicked
@@ -56,26 +83,50 @@ function flipCard (){
 
 	if(cardsInPlay.length === 2){
 		checkForMatch();
-		resetGame();
+		document.querySelector('button').addEventListener('click', resetGame);
 	}
 }
+
+
 //This function will create a new game board.
 function createBoard (){
 
+	document.getElementById('game-board').innerHTML = "";
+
 	for (var i = 0; i < cards.length ; i++) {
-		var cardElement = document.createElement('img')
+		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src','images/back.png');
 		cardElement.setAttribute('data-id',i);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
+//This function will create timed new game board
+function displayCards (){
 
-function resetGame (){
-	document.querySelector('button').addEventListener('click', createBoard);
+	createRandom();
+
+	for (var i = 0; i < cards.length ; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', cards[i].cardImage);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+	//timer
+	setTimeout("createBoard()",500);
 }
-createBoard();
-//flipCard(0);
-//flipCard(2);
+
+alert("Hello\nYou have 1 second to see cards\nMatch: score + 10\nNot matce: score - 5");
+displayCards();
+
+// function will reset cards 
+function resetGame(){
+	document.getElementById('game-board').innerHTML = "";
+
+	while( cardsInPlay.length !== 0 ){
+		cardsInPlay.pop();
+	};
+	displayCards();
+}
+
 
 
